d0887ad3c1ef930c84f26c1a05635de9c4768a07
Naira <nairaasaad23@gmail.com>
2024-12-06 00:26:21 +0800

    Imported Menu package to the Main class so its displayed

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -1,26 +1,25 @@
 package com.example.demo.controller;
 
-import java.lang.reflect.InvocationTargetException;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
+import com.example.demo.MainMenu;
 
 public class Main extends Application {
 
 	private static final int SCREEN_WIDTH = 1300;
 	private static final int SCREEN_HEIGHT = 750;
 	private static final String TITLE = "Sky Battle";
-	private Controller myController;
 
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+	public void start(Stage stage) {
 		stage.setTitle(TITLE);
 		stage.setResizable(false);
-		stage.setHeight(SCREEN_HEIGHT);
 		stage.setWidth(SCREEN_WIDTH);
-		myController = new Controller(stage);
-		myController.launchGame();
+		stage.setHeight(SCREEN_HEIGHT);
+
+		// Show the main menu
+		MainMenu mainMenu = new MainMenu(stage);
+		mainMenu.showMenu();
 	}
 
 	public static void main(String[] args) {
