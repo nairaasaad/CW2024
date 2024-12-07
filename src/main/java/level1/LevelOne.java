aafc41ccef1020be35ff9f95aec4cdbe77c19cbf
Naira <nairaasaad23@gmail.com>
2024-12-08 00:32:55 +0800

    Changed backgrounds to gifs
    - changed background1 and 2 to gifs for better aesthetics
    - added a new background to the main menu

---
+++ src/main/java/level1/LevelOne.java
@@ -0,0 +1,2 @@
+	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background1.gif";
+	private static final String NEXT_LEVEL = "level2.LevelTwo";
--- src/main/java/level2/LevelTwo.java
+++ src/main/java/level2/LevelTwo.java
@@ -1,1 +1,2 @@
+	private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background2.gif";
 			getRoot().getChildren().add(boss.getshieldImage());
