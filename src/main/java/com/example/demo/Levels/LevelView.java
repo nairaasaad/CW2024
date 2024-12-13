37423de3e87d154faba8570e9acf20c9b99f68bc
Naira <nairaasaad23@gmail.com>
2024-12-13 15:34:58 +0800

    UPDATED LevelView fixed a typo in the constant LOSS_SCREEN_Y_POSISITION to LOSS_SCREEN_Y_POSITION ensured that showWinImage and showGameOverImage avoid redundant additions to the root simplified the calculation in removeHearts

--- src/main/java/com/example/demo/Levels/LevelView.java
+++ src/main/java/com/example/demo/Levels/LevelView.java
@@ -12,7 +12,8 @@ public class LevelView {
 	private static final int WIN_IMAGE_X_POSITION = 355;
 	private static final int WIN_IMAGE_Y_POSITION = 175;
 	private static final int LOSS_SCREEN_X_POSITION = -50;
-	private static final int LOSS_SCREEN_Y_POSISITION = -350;
+	private static final int LOSS_SCREEN_Y_POSITION = -350;
+
 	private final Group root;
 	private final WinImage winImage;
 	private final GameOverImage gameOverImage;
@@ -20,9 +21,11 @@ public class LevelView {
 
 	public LevelView(Group root, int heartsToDisplay) {
 		this.root = root;
-		this.heartDisplay = new HeartDisplay(HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay);
+		this.heartDisplay = new HeartDisplay(
+				HEART_DISPLAY_X_POSITION, HEART_DISPLAY_Y_POSITION, heartsToDisplay
+		);
 		this.winImage = new WinImage(WIN_IMAGE_X_POSITION, WIN_IMAGE_Y_POSITION);
-		this.gameOverImage = new GameOverImage(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSISITION);
+		this.gameOverImage = new GameOverImage(LOSS_SCREEN_X_POSITION, LOSS_SCREEN_Y_POSITION);
 	}
 
 	public void showHeartDisplay() {
@@ -30,19 +33,22 @@ public class LevelView {
 	}
 
 	public void showWinImage() {
-		root.getChildren().add(winImage);
-		winImage.showWinImage();
-	}
-
-	public void showGameOverImage() {
-		root.getChildren().add(gameOverImage);
-	}
-
-	public void removeHearts(int heartsRemaining) {
-		int currentNumberOfHearts = heartDisplay.getContainer().getChildren().size();
-		for (int i = 0; i < currentNumberOfHearts - heartsRemaining; i++) {
-			heartDisplay.removeHeart();
+		if (!root.getChildren().contains(winImage)) {
+			root.getChildren().add(winImage);
+			winImage.showWinImage();
 		}
 	}
 
+	public void showGameOverImage() {
+		if (!root.getChildren().contains(gameOverImage)) {
+			root.getChildren().add(gameOverImage);
+		}
+	}
+
+	public void removeHearts(int heartsRemaining) {
+		int heartsToRemove = heartDisplay.getContainer().getChildren().size() - heartsRemaining;
+		for (int i = 0; i < heartsToRemove; i++) {
+			heartDisplay.removeHeart();
+		}
+	}
 }
